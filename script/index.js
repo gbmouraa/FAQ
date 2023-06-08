0f41e4a2746eba10a53cc76d8badbdd5ffad7e39
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-06-08 15:43:38 -0300

    Some changes

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.ask').forEach(question => question.addEventListener(
+document.querySelectorAll('.question').forEach(question => question.addEventListener(
     'click', () => {
         if(question.parentNode.classList.contains('active')){
             question.parentNode.classList.toggle('active')
